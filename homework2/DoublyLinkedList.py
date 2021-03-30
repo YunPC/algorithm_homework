d88e7d5b3ff2b94fb07ed91adcb793cf7708f0eb
YunPC <apsn2000@nate.com>
2021-03-30 10:05:03 +0900

    refactor: Erase method call, remove else
    예외 사항에서 바로 return하고 else문을 삭제하여 가독성 향상. self호출로 메소드를 호출하도록 변경

--- homework2/DoublyLinkedList.py
+++ homework2/DoublyLinkedList.py
@@ -19,109 +19,117 @@ class DoublyLinkedList:
         if self.head is None:
             self.head = Node(value, None, None)
             self.tail = self.head
-        else:
-            new_node = Node(value, self.head, None)
-            self.head.prev = new_node
-            self.head = new_node
+            return
+
+        new_node = Node(value, self.head, None)
+        self.head.prev = new_node
+        self.head = new_node
             
 
     def append(self, value):
         if self.head is None :
             self.head = Node(value, None, None)
             self.tail = self.head
-        else:
-            node = self.head
-            while node.next:
-                node = node.next
-            node.next = Node(value, None, node)
-            self.tail = node.next
+            return
+        
+        node = self.head
+        while node.next:
+            node = node.next
+        node.next = Node(value, None, node)
+        self.tail = node.next
 
     def set_head(self, index):
         if DoublyLinkedList.is_empty(self):
             print('SSL is empty!')
-        else:
-            node = self.head
-            idx = 0
-            while node:
-                if idx == index:
-                    self.head = node
-                    node.prev = None
-                    return
-                node = node.next
-                idx += 1
-            print('Invalid index check the size of SSL')
+            return
+        
+        node = self.head
+        idx = 0
+        while node:
+            if idx == index:
+                self.head = node
+                node.prev = None
+                return
+            node = node.next
+            idx += 1
+        print('Invalid index check the size of SSL')
 
     def set_tail(self, index):
         if DoublyLinkedList.is_empty(self):
             print('SSL is empty!')
-        else:
-            node = self.head
-            idx = 0
-            while node:
-                if idx == index:
-                    self.tail = node
-                    self.tail.next = None
-                    return
-                node = node.next
-                idx += 1
-            print('Invalid index check the size of SSL')
+            return
+        
+        node = self.head
+        idx = 0
+        while node:
+            if idx == index:
+                self.tail = node
+                self.tail.next = None
+                return
+            node = node.next
+            idx += 1
+        print('Invalid index check the size of SSL')
 
     def access(self, index):
         if DoublyLinkedList.is_empty(self):
             print('SSL is empty!')
-        else:
-            node = self.head
-            idx = 0
-            while node:
-                if idx == index:
-                    return node.value
-                node = node.next
-                idx += 1
-            print('Invalid index check the size of SSL')
+            return
+        
+        node = self.head
+        idx = 0
+        while node:
+            if idx == index:
+                return node.value
+            node = node.next
+            idx += 1
+        print('Invalid index check the size of SSL')
 
     def insert(self, index, value):
         node = self.head
         if index == 0:
             DoublyLinkedList.prepend(self, value)
-        else:
-            idx = 0
-            while node:
-                if idx+1 == index:
-                    new_node = Node(value, node.next, node)
-                    new_node.next = node.next
-                    new_node.prev = node
-                    node.next = new_node
-                    if new_node.next is not None:
-                        new_node.next.prev = new_node
-                    else:
-                        self.tail = new_node
-                    return
-                node = node.next
-                idx += 1
-            print('Invalid index check the size of SSL')
+            return
+        
+        idx = 0
+        while node:
+            if idx+1 == index:
+                new_node = Node(value, node.next, node)
+                new_node.next = node.next
+                new_node.prev = node
+                node.next = new_node
+                if new_node.next is not None:
+                    new_node.next.prev = new_node
+                else:
+                    self.tail = new_node
+                return
+            node = node.next
+            idx += 1
+        print('Invalid index check the size of SSL')
 
     def remove(self, index):
         if DoublyLinkedList.is_empty(self):
             print('SSL is empty!')
+            return
         if index == 0:
             self.head = self.head.next
             self.head.prev = None
-        else:
-            idx = 0
-            node = self.head
-            while node:
-                if idx+1 == index:
-                    del_node = node.next
-                    node.next = del_node.next
-                    if del_node.next is not None:
-                        del_node.next.prev = node
-                    else:
-                        self.tail = node
-                    return
-                node = node.next
-                idx += 1
-            print('Invalid index check the size of SSL')
+            return
         
+        idx = 0
+        node = self.head
+        while node:
+            if idx+1 == index:
+                del_node = node.next
+                node.next = del_node.next
+                if del_node.next is not None:
+                    del_node.next.prev = node
+                else:
+                    self.tail = node
+                return
+            node = node.next
+            idx += 1
+        print('Invalid index check the size of SSL')
+    
 
     def print(self):
         node = self.head
@@ -141,74 +149,66 @@ class DoublyLinkedList:
 ddl = DoublyLinkedList()
 
 print("----------------------is empty--------------------")
-print('DDL is empty : ' , DoublyLinkedList.is_empty(ddl))
+print('DDL is empty : ' , ddl.is_empty())
 
 print("----------------------prepend--------------------")
 print('prepend "1-4" into DLL : ', end = '')
 
 for i in range(1, 5):
-    DoublyLinkedList.prepend(ddl, i)
+    ddl.prepend(i)
 
-DoublyLinkedList.print(ddl)
-
-print("this DDL is empty : ", DoublyLinkedList.is_empty(ddl))
+ddl.print()
+print("this DDL is empty : ", ddl.is_empty())
 
 print("----------------------append--------------------")
 print('append 5 in SSL : ', end = '')
 
-DoublyLinkedList.append(ddl, 5)
+ddl.append(5)
 
-DoublyLinkedList.print(ddl)
-
+ddl.print()
 print("----------------------set head--------------------")
 print('set head at -1 (invalid) : ', end = '')
-DoublyLinkedList.set_head(ddl, -1)
+ddl.set_head(-1)
 
 print('set head at third : ', end = '')
-DoublyLinkedList.set_head(ddl, 2)
-DoublyLinkedList.print(ddl)
-
+ddl.set_head(2)
+ddl.print()
 print("----------------------set tail--------------------")
 print('set tail at -1 (invalid) : ', end = '')
-DoublyLinkedList.set_tail(ddl, -1)
+ddl.set_tail(-1)
 
 print('set tail at second one : ', end = '')
-DoublyLinkedList.set_tail(ddl, 1)
-DoublyLinkedList.print(ddl)
-
+ddl.set_tail(1)
+ddl.print()
 print("----------------------access--------------------")
 print('Access at -1 (invalid) : ', end = '')
-DoublyLinkedList.access(ddl, -1)
+ddl.access(-1)
 
 print('Access at last one : ', end = '')
-print(DoublyLinkedList.access(ddl, 1))
+print(ddl.access(1))
 
 print("----------------------insert--------------------")
 
 print('Insert -66 at -1 (invalid) : ', end = '')
-DoublyLinkedList.insert(ddl, -1, 66)
+ddl.insert(-1, 66)
 
 print('Insert 3 at first : ', end = '')
-DoublyLinkedList.insert(ddl, 0, 3)
-DoublyLinkedList.print(ddl)
-
+ddl.insert(0, 3)
+ddl.print()
 print('Insert 6 at last : ', end = '')
-DoublyLinkedList.insert(ddl, 3, 6)
-DoublyLinkedList.print(ddl)
-
+ddl.insert(3, 6)
+ddl.print()
 print("----------------------remove--------------------")
 
 print('Remove at -1 (invalid) : ', end = '')
-DoublyLinkedList.remove(ddl, -1)
+ddl.remove(-1)
 
 print('Remove first value  : ', end = '')
-DoublyLinkedList.remove(ddl, 0)
-DoublyLinkedList.print(ddl)
-
+ddl.remove(0)
+ddl.print()
 print('Remove last value : ', end = '')
-DoublyLinkedList.remove(ddl, 2)
-DoublyLinkedList.print(ddl)
-
+ddl.remove(2)
+ddl.print()
 print("----------------------print reverse--------------------")
 print('print reverse : ', end = '')
-DoublyLinkedList.print_reverse(ddl)
+ddl.print_reverse()
--- homework2/SinglyLinkedList.py
+++ homework2/SinglyLinkedList.py
@@ -16,77 +16,81 @@ class SinglyLinkedList:
     def prepend(self, value):
         if self.head is None :
             self.head = Node(value, None)
-        else:
-            new_node = Node(value, self.head)
-            self.head = new_node
+            return
+        new_node = Node(value, self.head)
+        self.head = new_node
 
     def append(self, value):
         if self.head is None :
             self.head = Node(value, None)
-        else:
-            node = self.head
-            while node.next:
-                node = node.next
-            node.next = Node(value, None)
+            return
+        node = self.head
+        while node.next:
+            node = node.next
+        node.next = Node(value, None)
 
     def set_head(self, index):
         if SinglyLinkedList.is_empty(self):
             print('SSL is empty!')
-        else:
-            node = self.head
-            idx = 0
-            while node:
-                if idx == index:
-                    self.head = node
-                    return
-                node = node.next
-                idx += 1
-            print('Invalid index check the size of SSL')
+            return
+        node = self.head
+        idx = 0
+        while node:
+            if idx == index:
+                self.head = node
+                return
+            node = node.next
+            idx += 1
+        print('Invalid index check the size of SSL')
 
     def access(self, index):
         if SinglyLinkedList.is_empty(self):
             print('SSL is empty!')
-        else:
-            node = self.head
-            idx = 0
-            while node:
-                if idx == index:
-                    return node.value
-                node = node.next
-                idx += 1
-            print('Invalid index check the size of SSL')
+            return
+        
+        node = self.head
+        idx = 0
+        while node:
+            if idx == index:
+                return node.value
+            node = node.next
+            idx += 1
+        print('Invalid index check the size of SSL')
 
     def insert(self, index, value):
         node = self.head
         if index == 0:
             SinglyLinkedList.prepend(self, value)
-        else:
-            idx = 0
-            while node:
-                if idx+1 == index:
-                    new_node = Node(value, node.next)
-                    node.next = new_node
-                    return
-                node = node.next
-                idx += 1
-            print('Invalid index check the size of SSL')
+            return
+    
+        idx = 0
+        while node:
+            if idx+1 == index:
+                new_node = Node(value, node.next)
+                node.next = new_node
+                return
+            node = node.next
+            idx += 1
+        print('Invalid index check the size of SSL')
 
     def remove(self, index):
         if SinglyLinkedList.is_empty(self):
             print('SSL is empty!')
+            return
         if index == 0:
             self.head = self.head.next
-        else:
-            idx = 0
-            node = self.head
-            while node:
-                if idx+1 == index:
-                    del_node = node.next
-                    node.next = del_node.next
-                    return
-                node = node.next
-                idx += 1
-            print('Invalid index check the size of SSL')
+            return
+        
+        idx = 0
+        node = self.head
+        while node:
+            if idx+1 == index:
+                del_node = node.next
+                node.next = del_node.next
+                return
+            node = node.next
+            idx += 1
+        print('Invalid index check the size of SSL')
 
     def print(self):
         node = self.head
@@ -99,65 +103,65 @@ class SinglyLinkedList:
 single_linked_list = SinglyLinkedList()
 
 print("----------------------is empty--------------------")
-print("this SLL is empty : ", SinglyLinkedList.is_empty(single_linked_list))
+print("this SLL is empty : ", single_linked_list.is_empty())
 
 print("----------------------prepend--------------------")
 print('prepend "1-4" into SLL : ', end = '')
 
 for i in range(1, 5):
-    SinglyLinkedList.prepend(single_linked_list, i)
+    single_linked_list.prepend(i)
 
-SinglyLinkedList.print(single_linked_list)
+single_linked_list.print()
 
 print("----------------------append--------------------")
 print('append 5 in SSL : ', end = '')
 
-SinglyLinkedList.append(single_linked_list, 5)
+single_linked_list.append(5)
 
-SinglyLinkedList.print(single_linked_list)
+single_linked_list.print()
 
 print("----------------------set head--------------------")
 print('set head at -1 (invalid) : ', end = '')
-SinglyLinkedList.set_head(single_linked_list, -1)
+single_linked_list.set_head(-1)
 
 print('set head at third : ', end = '')
-SinglyLinkedList.set_head(single_linked_list, 2)
+single_linked_list.set_head(2)
 
-SinglyLinkedList.print(single_linked_list)
+single_linked_list.print()
 
 print("----------------------access--------------------")
 print('Access at -1 (invalid) : ', end = '')
-SinglyLinkedList.access(single_linked_list, -1)
+single_linked_list.access(-1)
 
 print('Access at last one : ', end = '')
-print(SinglyLinkedList.access(single_linked_list, 2))
+single_linked_list.access(2)
 
 print("----------------------insert--------------------")
 
 print('Insert -66 at -1 (invalid) : ', end = '')
-SinglyLinkedList.insert(single_linked_list, -1, 66)
+single_linked_list(-1, 66)
 
 print('Insert 3 at first : ', end = '')
-SinglyLinkedList.insert(single_linked_list, 0, 3)
-SinglyLinkedList.print(single_linked_list)
+single_linked_list.insert(0, 3)
+single_linked_list.print()
 
 print('Insert 6 at last : ', end = '')
-SinglyLinkedList.insert(single_linked_list, 3, 6)
-SinglyLinkedList.print(single_linked_list)
+single_linked_list.insert(3,6)
+single_linked_list.print()
 
 print("----------------------remove--------------------")
 
 print('Remove at -1 (invalid) : ', end = '')
-SinglyLinkedList.remove(single_linked_list, -1)
+single_linked_list.remove(-1)
 
 print('Remove first value  : ', end = '')
-SinglyLinkedList.remove(single_linked_list, 0)
-SinglyLinkedList.print(single_linked_list)
+single_linked_list.remove(0)
+single_linked_list.print()
 
 print('Remove last value : ', end = '')
-SinglyLinkedList.remove(single_linked_list, 3)
-SinglyLinkedList.print(single_linked_list)
+single_linked_list.remove(3)
+single_linked_list.print()
 
 print('Remove second value : ', end = '')
-SinglyLinkedList.remove(single_linked_list, 1)
-SinglyLinkedList.print(single_linked_list)
+single_linked_list.remove(1)
+single_linked_list.print()
